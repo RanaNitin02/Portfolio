e76cfe57d63b4f479b5bbec440091b4037f1b7be
Nitin Rana <nitinrana0529@gmail.com>
2025-09-21 01:36:23 +0530

    Change profile image source in About component

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,7 +1,7 @@
 
 import { Typewriter } from 'react-simple-typewriter';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/portfolio-profile.jpg';
+import profileImage from '../../assets/portfolio.png';
 
 const About = () => {
   return (
